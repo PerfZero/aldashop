559b4258a5a3654b7af0ae64a794ff3c7a552dc7
PerfZero1 <gundenis1@gmail.com>
2025-08-29 21:21:04 +0300

    fix111111111112222111123344

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,12 +37,7 @@ export default function Header() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch(`/api/categories`, {
-          method: 'GET',
-          headers: {
-            'accept': 'application/json',
-          },
-        });
+        const response = await fetch(`/api/categories`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
