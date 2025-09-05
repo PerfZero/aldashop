2dbe3bc0acccd78f772d7b3facb08d053efe3647
PerfZero1 <gundenis1@gmail.com>
2025-09-05 17:34:21 +0300

    first commit11111111111111111111

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -262,7 +262,7 @@ export default function Reviews({ hasReviews = true, avgRating = 0, reviewsCount
               </div>
               <button className={styles.modal__close} onClick={handleCloseModal}>
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M0.5 0.5L15.5 15.5M15.5 0.5L0.5 15.5" stroke="#C1AF86" stroke-linecap="round" />
+  <path d="M0.5 0.5L15.5 15.5M15.5 0.5L0.5 15.5" stroke="#C1AF86" strokeLinecap="round" />
 </svg>
               </button>
             </div>
